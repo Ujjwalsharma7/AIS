5d699b590650a38906b58b13a5dc4e907085eaf4
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-12 12:20:32 +0530

    added firebase

--- src/firebase/fire.js
+++ src/firebase/fire.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA09wsNYYMmbDMHpoEeLROGTnS1Deh5Cho",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 export const db = getFirestore();
+export const resdb = getStorage();
